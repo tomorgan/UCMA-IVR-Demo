beaee50da6bcc925d573799d4162eace07880e81
Tom Morgan <tom.r.morgan@gmail.com>
2015-11-15 07:35:30 +0000

    added logging, user auth

--- LyncServer.cs
+++ LyncServer.cs
@@ -1,85 +1,72 @@
-﻿using LyncAsyncExtensionMethods;
+﻿using log4net;
+using LyncAsyncExtensionMethods;
 using Microsoft.Rtc.Collaboration;
-using Microsoft.Rtc.Collaboration.AudioVideo;
 using System;
 using System.Threading.Tasks;
 
-namespace UCMA_IVR_Demo
+namespace IMPowershell
 {
     public class LyncServer
     {
-        private string _appUserAgent = "IVR Demo";
-        private string _appID = "tom_ivr_demo";
+        private string _appUserAgent = "IM Powershell";
+        private string _appID = "IMPowershell";
         private CollaborationPlatform _collabPlatform;
         private ApplicationEndpoint _endpoint;
-        private Conversation _confConversation;
+    
 
         public event EventHandler<EventArgs> LyncServerReady = delegate { };
-        public event EventHandler<CallReceivedEventArgs<AudioVideoCall>> IncomingCall = delegate { };
+        public event EventHandler<CallReceivedEventArgs<InstantMessagingCall>> IncomingCall = delegate { };
+
+        private static readonly ILog log = LogManager.GetLogger(typeof(LyncServer));
 
         public async Task Start()
         {
             try
             {
-                Console.WriteLine("Starting Collaboration Platform");
+                log.Info("Starting Collaboration Platform");
                 ProvisionedApplicationPlatformSettings settings = new ProvisionedApplicationPlatformSettings(_appUserAgent, _appID);
+           
                 _collabPlatform = new CollaborationPlatform(settings);
-                _collabPlatform.RegisterForApplicationEndpointSettings(OnNewApplicationEndpointDiscovered);
+                _collabPlatform.RegisterForApplicationEndpointSettings(OnNewApplicationEndpointDiscovered);                
+                
                 await _collabPlatform.StartupAsync();
-                Console.WriteLine("Platform Started");
+
+                log.Info("Platform Started");
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Error establishing collaboration platform: {0}", ex.ToString());
+                log.ErrorFormat("Error establishing collaboration platform: {0}", ex.ToString());
             }
         }
 
         public async Task Stop()
         {
-            Console.WriteLine("Stopping Lync Server");
+            log.Info("Stopping Lync Server");
             await _endpoint.TerminateAsync();
             await _collabPlatform.ShutdownAsync();
         }
 
-        public async Task AddCallerToConference(AudioVideoCall call)
-        {
-            try
-            {
-                AudioVideoMcuSession mcu = _confConversation.ConferenceSession.AudioVideoMcuSession;
-                await mcu.TransferAsync(call, null);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-
-        }
+     
 
         private async void OnNewApplicationEndpointDiscovered(object sender, ApplicationEndpointSettingsDiscoveredEventArgs e)
         {
-            Console.WriteLine(string.Format("New Endpoint {0} discovered", e.ApplicationEndpointSettings.OwnerUri));
+            log.Info(string.Format("New Endpoint {0} discovered", e.ApplicationEndpointSettings.OwnerUri));
             _endpoint = new ApplicationEndpoint(_collabPlatform, e.ApplicationEndpointSettings);
-            _endpoint.RegisterForIncomingCall<AudioVideoCall>(OnIncomingCall);
+            _endpoint.RegisterForIncomingCall<InstantMessagingCall>(OnIncomingIM);
+            
             await _endpoint.EstablishAsync();
-            Console.WriteLine("Endpoint established");
-            await CreateConference();
-            LyncServerReady(this, new EventArgs());
+            log.Info("Endpoint established");
+           LyncServerReady(this, new EventArgs());
         }
 
-        //new incoming audio call
-        private void OnIncomingCall(object sender, CallReceivedEventArgs<AudioVideoCall> e)
+        //new incoming instant message conversation
+        private void OnIncomingIM(object sender, CallReceivedEventArgs<InstantMessagingCall> e)
         {
-            Console.WriteLine(string.Format("Incoming call! Caller: {0}", e.Call.RemoteEndpoint.Uri));
+            log.Info(string.Format("New Message from {0} : {1}", e.Call.RemoteEndpoint.Uri, e.ToastMessage.Message));
             IncomingCall(this, e);
         }
 
-        private async Task CreateConference()
-        {
-            _confConversation = new Conversation(_endpoint);
-            var options = new ConferenceJoinOptions() { JoinMode = JoinMode.TrustedParticipant, AdHocConferenceAccessLevel = ConferenceAccessLevel.Everyone, AdHocConferenceAutomaticLeaderAssignment = AutomaticLeaderAssignment.Everyone };
-            await _confConversation.ConferenceSession.JoinAsync(options);
-            Console.WriteLine("New conference created");
-        }
+      
 
 
     }
--- Program.cs
+++ Program.cs
@@ -1,67 +1,93 @@
-﻿using LyncAsyncExtensionMethods;
-using Microsoft.Rtc.Collaboration.AudioVideo;
+﻿using log4net;
 using System;
+using System.ServiceProcess;
 using System.Threading.Tasks;
 
 
-namespace UCMA_IVR_Demo
+namespace IMPowershell
 {
     class Program
     {
-        private static LyncServer _server;
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
 
+        #region Service bits
+        public const string ServiceName = "PowerShIM";
+
+        public class Service : ServiceBase
+        {
+            public Service()
+            {
+                ServiceName = Program.ServiceName;
+            }
+
+            protected override void OnStart(string[] args)
+            {
+                Program.Start(args);
+            }
+
+            protected override void OnStop()
+            {
+                Program.Stop();
+            }
+        }
+        #endregion
+
+        private static LyncServer _server;
+        
         static void Main(string[] args)
         {
-            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionTrapper;  //quick and dirty error handling :)
+          if (Environment.UserInteractive)
+            {
+                Start(args);
+                Console.WriteLine("Press any key to stop...");
+                Console.ReadKey(true);
+                Stop();
+            }
+          else
+            {
+                using (var service = new Service())
+                    ServiceBase.Run(service);
+            }
+        }
+
+        private static void Start(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionTrapper;
             _server = new LyncServer();
             _server.LyncServerReady += server_LyncServerReady;
             _server.IncomingCall += server_IncomingCall;
-            Task t = _server.Start();
-            
+            Task t = _server.Start();          
+        }
 
-            Console.ReadLine();
+        private static void Stop()
+        {
             var stopping = _server.Stop();
             stopping.Wait();
 
-            Console.WriteLine("Press any key to exit");
-            Console.ReadLine();
+            Console.WriteLine("Service Stopped. Exiting...");            
         }
 
         private static void UnhandledExceptionTrapper(object sender, UnhandledExceptionEventArgs e)
         {
-            Console.WriteLine(e.ExceptionObject.ToString());
-            Console.WriteLine("Press Enter to continue");
-            Console.ReadLine();
+            log.Error(e.ExceptionObject.ToString());
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine("Error: {0}", e.ExceptionObject);
+                Console.WriteLine("Press Enter to continue");
+                Console.ReadLine();
+            }
         }
 
         private static void server_LyncServerReady(object sender, EventArgs e)
         {
             Console.WriteLine("Lync Server Ready");
         }
-        
 
-        static void server_IncomingCall(object sender, Microsoft.Rtc.Collaboration.CallReceivedEventArgs<Microsoft.Rtc.Collaboration.AudioVideo.AudioVideoCall> e)
+
+        static void server_IncomingCall(object sender, Microsoft.Rtc.Collaboration.CallReceivedEventArgs<Microsoft.Rtc.Collaboration.InstantMessagingCall> e)
         {
-            e.Call.AudioVideoFlowConfigurationRequested += Call_AudioVideoFlowConfigurationRequested;
-            e.Call.AcceptAsync();
+            var ps = new PSRunner(e.Call, e.ToastMessage.Message);
+            ps.ParseAndExecute();
         }
-
-        static void Call_AudioVideoFlowConfigurationRequested(object sender, AudioVideoFlowConfigurationRequestedEventArgs e)
-        {
-            e.Flow.StateChanged +=Flow_StateChanged;
-        }
-
-        static void Flow_StateChanged(object sender, Microsoft.Rtc.Collaboration.MediaFlowStateChangedEventArgs e)
-        {
-            if (e.State == Microsoft.Rtc.Collaboration.MediaFlowState.Active)
-            {
-               
-                var flow = sender as AudioVideoFlow;
-                var ivr = new IVRMenu();
-                ivr.StartWithWelcome(flow, _server);
-            }
-        }
-
-    
     }
 }
